e5bcde098bd79ad6bb7fe5f51c861d9960ae32b0
Agata <agambin7@alu.ucam.edu>
2026-05-19 17:31:07 +0200

    login

--- src/main/java/edu/ucam/servlet/LoginServlet.java
+++ src/main/java/edu/ucam/servlet/LoginServlet.java
@@ -20,14 +20,17 @@ public class LoginServlet extends HttpServlet {
 
         String username = request.getParameter("username");
         String password = request.getParameter("password");
+        
+        System.out.println("Usuario recibido: " + username);
+        System.out.println("Password recibida: " + password);
+
 
         Usuario usuario = null;
 
-        try {
-            Connection conexion = ConexionBD.getConexion();
+        String sql = "SELECT * FROM usuarios WHERE username = ? AND password = ?";
 
-            String sql = "SELECT * FROM usuarios WHERE username = ? AND password = ?";
-            PreparedStatement ps = conexion.prepareStatement(sql);
+        try (Connection conexion = ConexionBD.getConexion();
+             PreparedStatement ps = conexion.prepareStatement(sql)) {
 
             ps.setString(1, username);
             ps.setString(2, password);
@@ -43,18 +46,12 @@ public class LoginServlet extends HttpServlet {
                 );
             }
 
-            rs.close();
-            ps.close();
-            conexion.close();
-
         } catch (Exception e) {
             e.printStackTrace();
         }
 
         if (usuario != null) {
-            HttpSession session = request.getSession();
-            session.setAttribute("usuario", usuario);
-
+            request.getSession().setAttribute("usuario", usuario);
             response.sendRedirect("secured/listarUsuarios.jsp");
         } else {
             request.setAttribute("error", "Usuario o contraseña incorrectos");
